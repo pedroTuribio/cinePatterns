1b8cefe1f5edf3034e6133498002607367a32e1f
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-06 11:50:20 -0300

    Implement main function for client and employee actions

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+from models.cliente import Cliente
+from models.funcionario import Funcionario
+
+def main():
+    cliente1 = Cliente("Pedro", "pedro@email.com")
+    funcionario1 = Funcionario("Mariana", "mariana@cinema.com", "Gerente")
+
+    cliente1.login()
+    cliente1.comprar_ingresso()
+
+    funcionario1.login()
+    funcionario1.cadastrar_filme("Homem-Aranha: Além do Aranhaverso")
+
+if __name__ == "__main__":
+    main()
