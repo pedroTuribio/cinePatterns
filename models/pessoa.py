a4b936a309be9ae18dae9a57b672f070426d8d80
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-06 11:36:58 -0300

    Add abstract Pessoa class for user representation

---
+++ models/pessoa.py
@@ -0,0 +1,22 @@
+# models/pessoa.py
+# Classe base abstrata para representar uma pessoa no sistema
+
+from abc import ABC, abstractmethod
+
+class Pessoa(ABC):
+    def __init__(self, nome: str, email: str):
+        self._nome = nome
+        self._email = email
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def email(self):
+        return self._email
+
+    @abstractmethod
+    def login(self):
+        """Método abstrato que deve ser implementado nas subclasses"""
+        pass
