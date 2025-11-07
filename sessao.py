61ea804d159d7e7c1c2bc1975db448e20a4607f5
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-07 12:05:34 -0300

    Create sessao.py

---
+++ sessao.py
@@ -0,0 +1,11 @@
+from modelos.filme import Filme
+from modelos.sala import Sala
+
+class Sessao:
+    def __init__(self, filme: Filme, sala: Sala, horario: str):
+        self.filme = filme
+        self.sala = sala
+        self.horario = horario
+
+    def exibir_info(self):
+        print(f"Sessão: {self.filme.titulo} - {self.horario} - Sala {self.sala.numero}")
