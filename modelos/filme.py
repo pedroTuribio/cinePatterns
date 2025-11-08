10cb85e2f03262d4cea6d74dccaa427dea9503cd
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-08 14:23:33 -0300

    Create filme.py

---
+++ modelos/filme.py
@@ -0,0 +1,5 @@
+class Filme:
+    def __init__(self, titulo: str, duracao: int, classificacao: str):
+        self.titulo = titulo
+        self.duracao = duracao
+        self.classificacao = classificacao
