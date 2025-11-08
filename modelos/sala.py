0161bc8c45dd7aec82a744a5f5a56bc73922f208
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-08 14:18:47 -0300

    Delete modelos directory

--- modelos/sala.py
+++
@@ -1,7 +0,0 @@
-class Sala:
-    def __init__(self, numero: int, capacidade: int):
-        self.numero = numero
-        self.capacidade = capacidade
-
-    def exibir_info(self):
-        print(f"Sala {self.numero} - Capacidade: {self.capacidade} lugares")
