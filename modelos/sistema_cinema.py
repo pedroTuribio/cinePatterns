bb05d45f948417fa0b8d6fa734e9ac1802935ea4
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-08 14:26:36 -0300

    Create sistema_cinema.py

---
+++ modelos/sistema_cinema.py
@@ -0,0 +1,30 @@
+from modelos.sessao import Sessao
+from modelos.filme import Filme
+from modelos.sala import Sala
+from modelos.observador.subject import Subject
+
+class SistemaCinema(Subject):
+    _instancia = None
+
+    def __init__(self):
+        if SistemaCinema._instancia is not None:
+            raise Exception("Use SistemaCinema.get_instance() para obter a instância do sistema.")
+        super().__init__()
+        self.sessoes = []
+
+    @staticmethod
+    def get_instance():
+        if SistemaCinema._instancia is None:
+            SistemaCinema._instancia = SistemaCinema()
+        return SistemaCinema._instancia
+
+    def adicionar_sessao(self, sessao: Sessao):
+        self.sessoes.append(sessao)
+        mensagem = f"Novo filme em cartaz: {sessao.filme.titulo} às {sessao.horario} (Sala {sessao.sala.numero})"
+        print(mensagem)
+        self.notificar_observadores(mensagem)
+
+    def listar_sessoes(self):
+        print("\n📅 Sessões disponíveis:")
+        for sessao in self.sessoes:
+            sessao.exibir_info()
