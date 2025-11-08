9db5da57987499d6cb0be024fc1c0fb73e189658
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-08 11:48:43 -0300

    Create pagamento_strategy.py

---
+++ modelos/pagamento/pagamento_strategy.py
@@ -0,0 +1,16 @@
+# modelos/pagamento/metodos_pagamento.py
+# Implementações concretas das estratégias de pagamento
+
+from modelos.pagamento.pagamento_strategy import PagamentoStrategy
+
+class PagamentoPix(PagamentoStrategy):
+    def pagar(self, valor: float):
+        print(f"Pagamento de R${valor:.2f} realizado via Pix ✅")
+
+class PagamentoCartao(PagamentoStrategy):
+    def pagar(self, valor: float):
+        print(f"Pagamento de R${valor:.2f} realizado no Cartão 💳")
+
+class PagamentoDinheiro(PagamentoStrategy):
+    def pagar(self, valor: float):
+        print(f"Pagamento de R${valor:.2f} realizado em Dinheiro 💵")
