26697a4284204386575a932320cd4d1ec9642c3d
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-08 14:22:48 -0300

    Create pessoa.py

---
+++ modelos/pessoa.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    def __init__(self, nome: str, email: str):
+        self.nome = nome
+        self.email = email
+
+    def exibir_info(self):
+        print(f"Nome: {self.nome} | Email: {self.email}")
