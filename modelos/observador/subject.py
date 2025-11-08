338c4e4138e465812a334b89f884b81f47699e9a
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-08 11:49:18 -0300

    Add Subject class for observer pattern implementation

---
+++ modelos/observador/subject.py
@@ -0,0 +1,16 @@
+# modelos/observador/subject.py
+# Define o sujeito que notifica os observadores
+
+class Subject:
+    def __init__(self):
+        self._observadores = []
+
+    def adicionar_observador(self, observador):
+        self._observadores.append(observador)
+
+    def remover_observador(self, observador):
+        self._observadores.remove(observador)
+
+    def notificar_observadores(self, mensagem: str):
+        for observador in self._observadores:
+            observador.atualizar(mensagem)
