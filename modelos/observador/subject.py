7eb3a02c59c1bff0c5af55176096be7530f9e2dd
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-11 20:41:06 -0300

    Update subject.py

--- modelos/observador/subject.py
+++ modelos/observador/subject.py
@@ -5,9 +5,6 @@ class Subject:
     def adicionar_observador(self, observador):
         self._observadores.append(observador)
 
-    def remover_observador(self, observador):
-        self._observadores.remove(observador)
-
     def notificar_observadores(self, mensagem: str):
         for observador in self._observadores:
             observador.atualizar(mensagem)
