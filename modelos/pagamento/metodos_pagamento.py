66727c0c61c45e0b27caed94626cc8b3738038d9
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-11 20:22:31 -0300

    Update metodos_pagamento.py

--- modelos/pagamento/metodos_pagamento.py
+++ modelos/pagamento/metodos_pagamento.py
@@ -1,13 +1,18 @@
 from modelos.pagamento.pagamento_strategy import PagamentoStrategy
 
+class PagamentoStrategy(ABC):
+    @abstractmethod
+    def pagar(self, valor: float):
+        pass
+
 class PagamentoPix(PagamentoStrategy):
     def pagar(self, valor: float):
-        print(f"Pagamento de R${valor:.2f} realizado via Pix ✅")
+        print(f"Pagamento de R${valor:.2f} realizado via Pix.")
 
 class PagamentoCartao(PagamentoStrategy):
     def pagar(self, valor: float):
-        print(f"Pagamento de R${valor:.2f} realizado no Cartão 💳")
+        print(f"Pagamento de R${valor:.2f} realizado no Cartão.")
 
 class PagamentoDinheiro(PagamentoStrategy):
     def pagar(self, valor: float):
-        print(f"Pagamento de R${valor:.2f} realizado em Dinheiro 💵")
+        print(f"Pagamento de R${valor:.2f} realizado em Dinheiro.")
