9285f119a12e4ae39217648133dbc720d563f189
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-11 20:19:10 -0300

    Update funcionario.py

--- modelos/funcionario.py
+++ modelos/funcionario.py
@@ -1,9 +1,7 @@
-from modelos.pessoa import Pessoa
-
 class Funcionario(Pessoa):
     def __init__(self, nome: str, email: str, cargo: str):
         super().__init__(nome, email)
         self.cargo = cargo
 
     def exibir_info(self):
-        print(f"Funcionario: {self.nome} | Cargo: {self.cargo}")
+        print(f"Funcionário: {self.nome} | Cargo: {self.cargo}")
