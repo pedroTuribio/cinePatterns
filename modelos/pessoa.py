3012120f9fe435948e8df3700b077e829b011045
Pedro Carvalho <124682237+pedroTuribio@users.noreply.github.com>
2025-11-24 12:02:28 -0300

    Update pessoa.py

--- modelos/pessoa.py
+++ modelos/pessoa.py
@@ -1,6 +1,4 @@
-  def __init__(self, nome: str, email: str):
+ class Pessoa:
+    def __init__(self, nome: str, email: str):
         self.nome = nome
         self.email = email
-
-    def exibir_info(self):
-        print(f"Nome: {self.nome} | Email: {self.email}")
